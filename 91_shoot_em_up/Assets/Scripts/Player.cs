183286a7c29b54018607f97f3c1ed8327fcfb4b7
Ricardo <rp_mira@hotmail.com>
2025-11-27 11:22:54 -0300

    Tiro esta funcionando

--- 91_shoot_em_up/Assets/Scripts/Player.cs
+++ 91_shoot_em_up/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;   // Interagir com UI
 
 [System.Serializable]   // Para conseguir editar pelo editor da unity
 public class Boundary   // São os limites que player pode ir pela tela
@@ -15,9 +16,22 @@ public class Player : MonoBehaviour
 
     public Boundary boundary;   // Classe que foi criada a cima
 
+    public GameObject bullet;   // Tiro do player
+
+    // UI botão de tiro
+    public Button shootButton;      // referência ao botão
+    public GameObject bulletPrefab; // prefab da bala
+    public Transform[] shotSpawns;  // ponto de saída da bala
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        shootButton.onClick.AddListener(Shoot); // Vincula a função Shoot ao clique do botão
+    }
+
+    void Update()
+    {
+
     }
 
     private void FixedUpdate()
@@ -34,4 +48,10 @@ public class Player : MonoBehaviour
         rb.position = new Vector2(Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax));  // Limita movimentação do player pela tela nos eixos x e y
 
     }
+
+    void Shoot()    // Efetua o tiro
+    {
+        Instantiate(bulletPrefab, shotSpawns[0].position, shotSpawns[0].rotation);  // Instancia a bala no ponto de tiro
+    }
+
 }
---
+++ 91_shoot_em_up/Assets/Scripts/Reuse/Mover.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;     // Velocidade do tiro
+
+    private Rigidbody2D rb; // Gravidade
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();               // Acessa componente do obj
+
+        rb.linearVelocity = Vector2.up * speed;         // Faz obj ir para cima
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
