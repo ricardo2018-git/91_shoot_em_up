e2d4323ea06c4dcce86400676cc80d9356a9dca4
Ricardo <rp_mira@hotmail.com>
2025-12-22 11:04:46 -0300

    Musicas cenario ok

--- 91_shoot_em_up/Assets/Scripts/Scenario/MusicPlayer.cs
+++ 91_shoot_em_up/Assets/Scripts/Scenario/MusicPlayer.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class MusicPlayer : MonoBehaviour
 {
     private static MusicPlayer musicPlayer;     // Referencia a proprio class
+    private AudioSource audioSource;            // Referencia ao componente
 
     public AudioClip[] songs;                   // Vetor para receber todas as músicas
+    private int index;                          // Controla qual musica
 
     void Awake()
     {
@@ -21,11 +23,25 @@ public class MusicPlayer : MonoBehaviour
 
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();  // Acessa componente de audio
+        index = Random.Range(0, songs.Length);      // Sorteia um valor de 0 até a quantidade total de músicas que tiver no array
+        StartMusicPlayer();                         // Chama metodo que toca música do cenario
     }
 
     void Update()
     {
         
+    }
+
+    void StartMusicPlayer()                 // Toca todas as músicas
+    {
+        audioSource.clip = songs[index];    // Seleciona uma música
+        index++;                            // Add +1, ou seja vai para proxima música
+        if(index >= songs.Length)           // Verifica se index ja passou da ultima musica
+        {
+            index = 0;                      // Reseta index, coloca na primeira música
+        }
+        audioSource.Play();                 // Coloca para tocar música
+        Invoke("StarMusicPlayer", audioSource.clip.length + 0.5f);  // Chama proprio metodo para tocar proxima música, mas só depois que terminar a que esta tocando + um preve atraso de 0.5f
     }
 }
